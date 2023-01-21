d8b94fae8c2ee617aa40ddd412abbc4023cd4da4
ZiedChekir <ziedchekir1@gmail.com>
2023-01-21 22:46:04 +0100

    Initial commit

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,23 @@
-console.log("Hello World!", browser);
+const button = document.getElementById('button');
+button.addEventListener('click',  async (e) => {
+    SwitchMode()
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+    browser.action.onClicked.addListener(SwitchMode);
+
+})
+
+async function SwitchMode() {
+  try {
+    await browser.scripting.insertCSS({
+      target: {
+        tabId: (await browser.tabs.getCurrent()).id,
+      },
+    css: `body {  background-color: black !important;}`,
+     
+    });
+  } catch (err) {
+    console.error(`failed to insert CSS: ${err}`);
+  }
+}
